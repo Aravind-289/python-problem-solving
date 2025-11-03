921db53e97b4e06342fabec4ef7bc84572c8253d
Aravind kukatla <aravindkukatla289@gmail.com>
2025-11-03 11:33:47 +0530

    Add files via upload

---
+++ problemsolving1.py/numberfunctions.py
@@ -0,0 +1,17 @@
+#1
+# list=[5,3,7,8,12,9]
+# max=0
+# for i in list:
+#     if i>max:
+#         max=i
+# print(max)
+#2
+# list=[5,3,7,8,12,9]
+# min=10
+# for i in list:
+#     if i<min:
+#         min=i
+# print(min)
+#3
+# print(min([5,3,7,8,12,9]))
+
---
+++ problemsolving1.py/problemsolving.py
@@ -0,0 +1,116 @@
+# 1
+
+# 2
+# a=int(input('enter your number'))
+# b=int(input('enter your number'))
+# x=a
+# y=b
+# while b != 0:
+#     remainder = a % b
+#     a = b
+#     b = remainder
+# gcd=a
+# lcm = (x * y) // gcd
+# print("GCD =", gcd)
+# print("LCM =", lcm)
+
+# 3
+# num1=int(input("enter your nuber"))
+# def print(n):
+#     return True
+# while True:i
+# if i in (a):
+#     print(a)
+
+# experence=int(input('Enter your experence:'))
+# if experence>15:
+#     print("salary is 50k")
+# elif experence>10:
+#     print("salry is 40k")
+# elif experence>5:
+#     print("salary is 30k")
+# else:
+#     print("salary is 20k")
+
+# num1=int(input("enter your number:"))
+# if num1%3==0 and num1%5==0:
+#     print("it is divisable by 3 and 5")
+# elif num1%3==0:
+#     print('It is divisable by 3')
+# elif num1%5==0:
+#     print('It is divisable by 5')
+
+
+# num=int(input('enter your number'))
+# count=1
+# for i in range(1,num+1):
+#     count=count*i
+# print(count)
+
+# list=[2,3,5,6,7]
+# sum=0
+# for i in list:
+#  sum+=i
+# print(sum)/
+# multiple values to mulatipule varables
+# num1,num2,num3="aravind",'raju','lavanya'
+# print(num3)
+# #assigning multiple varable to one value
+# num1=num2=num3="kukatla"
+# print(num2)
+# #indexing
+# tuple1=['aravind','ajay','nikhil','raju','lavanya']
+# print(tuple1[-3])
+# #complex number
+# cum1=3+4j
+# cum2=5+6j
+# print(cum1+cum2)
+# #type casting
+# str1="aravind kukatla"
+# print(list(str1))
+# #type conversion
+# print(1+False)
+# print(1+True)
+# #1
+# str1="aravind kukatla"
+# print(set(str1))
+# #2
+# num1=int(input('enter a number :'))
+# if num1%3==0 and num1%5==0:
+#     print("divisable of 3,5")
+# elif num1%3==0:
+#     print("divisable of 3")
+# elif num1%5==0:
+#     print("divisable of 5")
+# else:
+#     ('not divisable')
+# #3 identity operator
+# num1=float(input('enter your number :'))
+# if type(num1) is int:
+#     print('it is intiger type')
+# else:
+#     print('it is not a intiger type')
+
+# courses=['pythoon','java','dsa','cyber security','aritificial intaligence']
+# if 'pythoon' in courses:
+#     print('this is avalable')
+# else:
+#     print('not avalable')
+
+# list1=['Aravind','chaitanya demmed to be university','8.5']
+# list1.append('waranagal')
+# print(list1)
+
+# tuple=(23,45.5,'Aravind',4>5,None,(12,34.5,'raju'),[98,34.89,'niki'])
+# print(tuple[5][2])
+
+# 1
+# num1=12
+# num2=8.5
+# add=num1+num2
+# print (type(add))
+# 2
+# str1='25'
+# print(float(str1))
+# str='mahesh'
+# print(list(str))
